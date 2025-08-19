d63ce311b79951f180dbc565d1d2141da9377e0c
Seongchan-CHOI <chltjdcks329@naver.com>
2025-08-19 14:04:10 +0900

    초기 환경 설정 완료

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 function App() {
-  return (
-    <div>
-      App
-    </div>
-  );
+    return (
+        <div>
+            <p>App</p>
+        </div>
+    );
 }
 
 export default App;
